09a511b35a7e3a3b04ad7bf81ed2b0c23fd475ca
Luis Herrera <lherrera@gignosoft.cl>
2017-04-24 08:27:27 -0300

    commit antes de subir a github

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -2,18 +2,18 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Authentication Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used during authentication for various
+	| messages that we need to display to the user. You are free to modify
+	| these language lines according to your application's requirements.
+	|
+	*/
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Las credenciales introducidas son incorrectas.',
+    'throttle' => 'Demasiados intentos de acceso. Inténtelo de nuevo en :seconds segundos.',
 
 ];
--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -2,21 +2,21 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reset Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Las contraseñas deben contener al menos 6 caracters y coincidir.',
+    'reset' => 'Su contraseña ha sido cambiada.',
+    'sent' => 'Le hemos enviado un correo de cambio de contraseña!',
+    'token' => 'El código de cambio de contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con ese nombre.",
 
 ];
